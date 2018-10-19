7268aaca2a10c349f30f7f86f903ec7180ee53bb
rutz <jokeryoseob@gmail.com>
2018-10-19 18:56:37 +0700

    lab_device6

--- app/src/main/java/com/example/csitgis/lab6_sqlite/DatabaseHelper.java
+++ app/src/main/java/com/example/csitgis/lab6_sqlite/DatabaseHelper.java
@@ -1,4 +1,38 @@
 package com.example.csitgis.lab6_sqlite;
 
-public class DatabaseHelper {
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+    private static final int DATABASE_VERSION = 1;
+    private static final String DATABASE_NAME ="student_db";
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(Student.CREATE_TABLE);
+
+    }
+    @Override
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db,int i, int il) {
+        db.execSQL("DROP TABLE IF EXISTS " + Student.TABLE_NAME);
+        onCreate(db);
+    }
+    public void inserStudent(String id, String student){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+
+        values.put(Student.COLUMM_ID, id);
+        values.put(Student.COLUMN_NAME, student);
+
+        db.insert(Student.TABLE_NAME, null, values);
+        db.close();
+    }
 }
+
--- app/src/main/java/com/example/csitgis/lab6_sqlite/Student.java
+++ app/src/main/java/com/example/csitgis/lab6_sqlite/Student.java
@@ -1,4 +1,29 @@
 package com.example.csitgis.lab6_sqlite;
 
 public class Student {
+    public static final String TABLE_NAME = "student";
+
+    public static final String COLUMM_ID  ="std_id";
+    public static final String COLUMN_NAME ="std_name";
+
+    private String id;
+    private String name ;
+
+    public static final String CREATE_TABLE =
+            "CREATE TABLE " + TABLE_NAME + "("
+            + COLUMM_ID + "TEXT,"
+            + COLUMN_NAME + "TEXT"
+            + ")" ;
+    public Student(String id, String name) {
+        this.id= id;
+        this.name = name;
+    }
+    public Student() {}
+
+    public String getId() { return id; }
+    public void setId(String id) { this.id = id; }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name= name; }
+
 }
